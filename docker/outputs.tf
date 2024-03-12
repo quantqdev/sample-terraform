c383211f6a090b6696cce6982ab195eb4a72dc2f
Quan Ta <quantq.dev@gmail.com>
2024-03-12 17:57:54 +0700

    Docker output local variables

--- docker/outputs.tf
+++ docker/outputs.tf
@@ -3,6 +3,10 @@ output "container_id" {
   value       = docker_container.nginx.id
 }
 
+output "container_name" {
+  value = local.container_name
+}
+
 output "image_id" {
   description = "Docker image ID"
   value       = docker_image.nginx.id
