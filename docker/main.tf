717a728a47ba5fdb035aa4e79ae903539386cda7
Quan Ta <quantq.dev@gmail.com>
2024-03-14 10:08:37 +0700

    Docker: add check

--- docker/main.tf
+++ docker/main.tf
@@ -4,12 +4,22 @@ terraform {
       source  = "kreuzwerker/docker"
       version = "~> 3.0.1"
     }
+    time = {
+      source  = "hashicorp/time"
+      version = "0.11.1"
+    }
+    http = {
+      source  = "hashicorp/http"
+      version = "3.4.2"
+    }
   }
 }
 
 provider "docker" {
   host = "unix:///Users/quanta/.colima/default/docker.sock"
 }
+provider "time" {}
+provider "http" {}
 
 locals {
   container_name = "${var.container_name}-${var.env}"
@@ -27,5 +37,24 @@ resource "docker_container" "nginx" {
   ports {
     internal = 80
     external = var.container_port
+  }
+}
+
+resource "time_sleep" "wait_container_running" {
+  depends_on      = [docker_container.nginx]
+  create_duration = "2s"
+}
+
+check "response" {
+  data "http" "nginx" {
+    url        = "http://localhost:${var.container_port}"
+    insecure   = true
+    depends_on = [time_sleep.wait_container_running]
+  }
+
+  assert {
+    condition     = data.http.nginx.status_code == 200
+    error_message = "Nginx response is ${data.http.nginx.status_code}"
+
   }
 }
